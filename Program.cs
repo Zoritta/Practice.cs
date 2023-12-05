396d0a454f7bdc4286581efb16db05542765ec5b
Zoritta <zohreh.sadeghi@polestar.com>
2023-12-05 21:13:32 +0100

    applying methods in class array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    
+    
+        static void Main(string[] args )
+        {
+            int[] numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+            Console.WriteLine(" Length: " + numbers.Length);
+        }

b8e97e92bf4ca577b69ef722ab639f82b9a40119
Zoritta <zohreh.sadeghi@polestar.com>
2023-12-10 12:47:06 +0100

    ex 3

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿
+
+internal class Program
 {
-        static void Main(string[] args )
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>();
+        while (numbers.Count < 5)
         {
-            int[] numbers = new int[] { 3, 7, 9, 2, 14, 6 };
-            Console.WriteLine(" Length: " + numbers.Length);
+            Console.WriteLine("Please enter a number that you have not entered before: ");
+            var enteredNumber = Convert.ToInt32(Console.ReadLine());
+            if (numbers.Contains(enteredNumber))
+            {
+                Console.WriteLine("You have already entered " + enteredNumber);
+                continue;
+            }
+
+            numbers.Add(enteredNumber);
         }
+
+        numbers.Sort();
+        Console.WriteLine();
+        Console.WriteLine("These are the numbers you entered and I sorted:");
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}

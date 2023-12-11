bbc32c651091c44d08606c19933ca3c202a149aa
Zoritta <zohreh.sadeghi@polestar.com>
2023-12-11 11:24:57 +0100

    using StringBuilder to manipulate strings

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Practice.cs;
 using System;
+using System.Text;
 
 namespace Practice.cs
 {
@@ -7,10 +8,17 @@ namespace Practice.cs
     {
         static void Main(string[] args)
         {
-            var sentence = "This is going to be a really really really long text.";
-            var summary = StringUtility.SummerizedText(sentence);
-            Console.WriteLine(summary);
+            var builder = new StringBuilder();
+            builder.Append('-', 10)
+            .AppendLine()
+            .AppendLine("My Program")
+            .Append('-', 10)
+            .AppendLine()
+            .Replace('-', '+')
+            .Remove(0, 10)
+            .Insert(0, new string('*', 10));
 
+            Console.WriteLine(builder);
         }
 
     }

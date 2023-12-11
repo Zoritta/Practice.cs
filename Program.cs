393bd470a6bdcefec30a40d9c0e926b21d49bca8
Zoritta <zohreh.sadeghi@polestar.com>
2023-12-11 19:58:21 +0100

    refining your code by having logic methods and console methods seperated

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
 ﻿using Practice.cs;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Practice.cs
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append('-', 10)
-            .AppendLine()
-            .AppendLine("My Program")
-            .Append('-', 10)
-            .AppendLine()
-            .Replace('-', '+')
-            .Remove(0, 10)
-            .Insert(0, new string('*', 10));
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine(reversed);
+        }
 
-            Console.WriteLine(builder);
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+            return new string(array);
         }
 
     }

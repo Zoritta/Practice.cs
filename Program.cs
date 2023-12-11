01ba0b8e128d3bf100fc27c5a7f4ac202ad51952
Zoritta <zohreh.sadeghi@polestar.com>
2023-12-11 08:56:07 +0100

    improving code by cleaning

--- Program.cs
+++ Program.cs
@@ -5,27 +5,31 @@ class Program
     static void Main(string[] args)
     {
         var sentence = "This is going to be a really really really long text.";
-        const int maxLength = 18;
+        var summary = SummerizedText(sentence);
+        Console.WriteLine(summary);
 
-        if (sentence.Length < maxLength)
-            Console.WriteLine(sentence);
-        else
-        {
-            var words = sentence.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-
-            foreach ( var word in words )
-            {
-                summaryWords.Add(word);
-
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-
-            var summary = String.Join(" ", summaryWords) + "...";
-            Console.WriteLine(summary);
-        }
     }
+
+    static string SummerizedText(string text, int maxLength = 18)
+    {
+        if (text.Length < maxLength)
+            return text;
+        
+        
+        var words = text.Split(' ');
+        var totalCharacters = 0;
+        var summaryWords = new List<string>();
+
+        foreach (var word in words)
+        {
+            summaryWords.Add(word);
+
+            totalCharacters += word.Length + 1;
+            if (totalCharacters > maxLength)
+                break;
+        }
+
+        return String.Join(" ", summaryWords) + "...";
+    }
+
 }
